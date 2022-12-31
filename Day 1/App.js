c7c69941e27f0d065a20b3d72973162641950f45
harsha-commit <harshavardhan.bashavathini@gmail.com>
2022-12-31 11:29:11 +0530

    first commit

---
+++ Day 1/App.js
@@ -0,0 +1,31 @@
+// Creating a React element
+// {id: "title"} => <h1 id="title">Namaste from React </h1>
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "title1",
+  },
+  "Heading 1"
+);
+
+const heading2 = React.createElement(
+  "h1",
+  {
+    id: "title2",
+  },
+  "Heading 2"
+);
+
+const container = React.createElement("div", { className: "headings" }, [
+  heading1,
+  heading2,
+]);
+
+// React element is a JS object !!
+console.log(container);
+
+// Creating a Root (a single point of origin (for building) kind of thing)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// passing a React element inside the root
+root.render(container);
