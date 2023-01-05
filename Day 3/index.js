e835cad35f22adf1437d8d41b3150cb73ba051d0
harsha-commit <harshavardhan.bashavathini@gmail.com>
2023-01-06 02:51:32 +0530

    Day 3 Updated

--- Day 3/index.js
+++ Day 3/index.js
@@ -1,6 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
+// HTML
 /* <div class="header">
   <h1>Namaste React</h1>
   <ul>
@@ -9,24 +9,70 @@ import ReactDOM from "react-dom/client";
   </ul>
 </div> */
 
-// Damn, this became a MESS !! WE WANT JSX !!
-const container = React.createElement("div", { className: "headings" }, [
-  React.createElement(
-    "h1",
-    {
-      id: "title",
-      key: "h1",
-    },
-    "Namaste React"
-  ),
-  React.createElement("ul", {}, [
-    React.createElement("li", {}, "About Us"),
-    React.createElement("li", {}, "Contact"),
-  ]),
-]);
+// Coding without JSX 🥲
+// const container = createElement("div", { className: "headings" }, [
+//   createElement(
+//     "h1",
+//     {
+//       id: "title",
+//       key: "h1",
+//     },
+//     "Namaste React"
+//   ),
+//   createElement("ul", {}, [
+//     createElement("li", {}, "About Us"),
+//     createElement("li", {}, "Contact"),
+//   ]),
+// ]);
 
-// Here comes JSX 🥳
+// HERE COMES JSX 🥳
+// This is NOT HTML inside JS, the below code is HTML-like syntax
+// babel understands JSX
+// JSX uses React.createElement => Object => HTML(DOM)
+// parcel gives you babel
+// This is a React Element
+const heading = (
+  <h1 id="title" key="h1" className="namaste">
+    Namaste React
+  </h1>
+);
+
+const Title = () => (
+  <h1 id="title" key="t1" className="namaste">
+    Hello I'm the Title
+  </h1>
+);
+
+const Footer = () => (
+  <h1 id="title" key="f1" className="namaste">
+    Hello I'm the Footer
+  </h1>
+);
+
+// 34-38 => JSX Expression
+
+// React Component
+// - Functional Component - NEW
+// - Class Based Component
+
+// Functional Component (a JS Function that returns JSX)
+// All component names should start with a capital letter (convention)
+// does sanitization, so don't worry about hackers
+// <Title/> is known as Component Composition (Component in a component)
+const HeaderComponent = () => {
+  return (
+    <div>
+      {heading}
+      <Title />
+      {Footer()}
+      {console.log("Wait, What ??")}
+      <h2>Namaste from Functional Component</h2>
+      <h2>This is a h2 tag</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+// for functional component, this is the syntax
+root.render(<HeaderComponent />);
